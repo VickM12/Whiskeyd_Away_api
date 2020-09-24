fdba9c9c22905827db3610c51d583097c9e83713
Vicktor M <vicktor.moberg@gmail.com>
2020-09-24 14:29:16 -0400

    Deployed and seeded heroku db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Whiskey.create(name: 'Bulleit Bourbon', distiller: 'Bulleit Distilling CO.', origin: 'Kentucky, USA')
-Whiskey.create(name: 'Jameson Black Barrel', distiller: 'Jameson Distillery', origin: 'Dublin, Ireland')
-Whiskey.create(name: 'Bushmills Irish Whiskey', distiller: 'The "Old Bushmills" Distillery Co.', origin: 'Ireland')
-Whiskey.create(name: 'Bourbon Whiskey', distiller: 'New York State Yankee Distillers', origin: 'Saratoga County, New York, USA')
-Whiskey.create(name: 'Redbreast 15 Year Old', distiller: "Irish Distillers (Originally Gilbey's)", origin: "Ireland")
-Whiskey.create(name: 'Jim Beam Kentucky Straight Bourbon Whiskey', distiller: 'James B. Beam Distilling CO.', origin: 'Kentucky USA') 
+Whiskey.create(name: 'Bulleit Bourbon', distiller: 'Bulleit Distilling CO.', origin: 'Kentucky, USA', image: "https://cdn.shopify.com/s/files/1/0060/7558/0489/products/image_79115f05-3661-4423-a59b-0bb52a251963_480x.jpg?v=1543250011")
+Whiskey.create(name: 'Jameson Black Barrel', distiller: 'Jameson Distillery', origin: 'Dublin, Ireland', image: "https://cdn.shopify.com/s/files/1/1580/4729/products/Jameson_black_barrel.jpg?v=1516388051")
+Whiskey.create(name: 'Bushmills Irish Whiskey', distiller: 'The "Old Bushmills" Distillery Co.', origin: 'Ireland', image: "https://cdn2.bigcommerce.com/server5500/tpbc2s65/products/556/images/8490/BushWhite__12495.1496876549.1280.1280.jpg?c=2")
+Whiskey.create(name: 'Bourbon Whiskey', distiller: 'New York State Yankee Distillers', origin: 'Saratoga County, New York, USA', image: "https://static.wixstatic.com/media/9533c2_aaafa87bbc8540c9a5208d0d34765311~mv2_d_4320_3240_s_4_2.jpg/v1/crop/x_670,y_0,w_3017,h_3002/fill/w_262,h_260,al_c,q_80,usm_0.66_1.00_0.01/DSCN3864_JPG.webp")
+Whiskey.create(name: 'Redbreast 15 Year Old', distiller: "Irish Distillers (Originally Gilbey's)", origin: "Ireland", image: "https://cdn.shopify.com/s/files/1/0276/7778/6184/products/31540_600x600_crop_center.jpg?v=1595918371")
+Whiskey.create(name: 'Jim Beam Kentucky Straight Bourbon Whiskey', distiller: 'James B. Beam Distilling CO.', origin: 'Kentucky USA', image: "https://cdn1.masterofmalt.com/whiskies/p-2813/jim-beam-white-label-whiskey.jpg?ss=2.0") 
+Whiskey.create(name: "Blanton's Single Barrel Bourbon",
+  distiller: "Buffalo Trace ",
+  origin: "Kentucky, USA",
+  image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTdG2JY9RFiS9VZHgdEp0KQHpw3AurhichA5g&usqp=CAU")
