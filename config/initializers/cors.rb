2535c1d642b6492cde6f0492555c1f0081852399
Vicktor M <vicktor.moberg@gmail.com>
2020-09-24 15:05:57 -0400

    changed cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', 'https://whiskeyd-away.herokuapp.com/'
+    origins 'https://whiskeyd-away.herokuapp.com/'
 
     resource '*',
       headers: :any,
