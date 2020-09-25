aa0d3e823c157f7a95e1fd719c9ae500a228aded
Vicktor M <vicktor.moberg@gmail.com>
2020-09-25 18:54:01 -0400

    authentication works

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
   root 'welcome#index'
   resources :whiskeys
+
+  resources :users do                                                            
+    collection do
+      post '/login', to: 'users#login'                                            
+    end                                                  
+  end                                                                                
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
